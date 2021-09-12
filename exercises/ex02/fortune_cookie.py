f919942a4d20bc6b89a28f55b0cb23a2dda30ed7
Kaegon Matlock <kaegon@unc.edu>
2021-09-12 16:54:28 -0400

    commit 9.12

--- exercises/ex02/fortune_cookie.py
+++ exercises/ex02/fortune_cookie.py
@@ -8,10 +8,13 @@ print("Your fortune cookie says...")
 fortune: int = randint(1, 4)
 if fortune == 1:
     print("You will get an A on your next exercise!!")
-elif fortune == 2:
-    print("You will become a hacker one day!!")
-elif fortune == 3:
-    print("You will live forever!")
-elif fortune == 4:
-    print("Someone loves you... <3")
+else:
+    if fortune == 2:
+        print("You will become a hacker one day!!")
+    else:
+        if fortune == 3:
+            print("You will live forever!")
+        else:
+            if fortune == 4:
+                print("Someone loves you... <3")
 print("Now, go spread positive vibes!")
